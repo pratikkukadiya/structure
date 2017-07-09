dad01c4e0101e9d3dedc841cc0dbf0bcbeac5a1e
pratikkukadiya <pratik.ios.devloper@gmail.com>
2017-07-09 12:33:32 +0530

    pk

--- TestProject/ViewControllers/ViewController.swift
+++ TestProject/ViewControllers/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
         
         self.doSetUpScreenNavigationBar()
         self.doSetUpScreen()
